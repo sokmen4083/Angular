9e68ec9a6967148294247c134f273c1dee9e337d
Mehmet Sökmen <sokmen4083@gmail.com>
2020-10-20 00:33:00 +0200

    Promise Part 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,11 +24,11 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.dishservice.getFeaturedDish()
-    .then(dish => this.dish = dish);
+    .subscribe(dishes => this.dish = dishes);
     this.promotionservice.getFeaturedPromotion()
-    .then(promotion => this.promotion =promotion);
+    .subscribe(promotion => this.promotion =promotion);
     this.leaderService.getFeaturedLeader()
-    .then(leader => this.leader = leader);
+    .subscribe(leader => this.leader = leader);
   }
 
 }
