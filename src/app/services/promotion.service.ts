93576257fa2b81c5dd1414ded25afc213a526dd1
Mehmet Sökmen <sokmen4083@gmail.com>
2020-10-20 00:09:22 +0200

    Promise Part 2

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -7,15 +7,24 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotions(): Promise<Promotion[]> {
-    return Promise.resolve(PROMOTIONS);
+  getPromotiones(): Promise<Promotion[]> {
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(PROMOTIONS), 2000);
+    });
   }
 
-  getPromotion(id: number): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]);
+  getPromotion(id: string): Promise<Promotion> {
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedPromotion(): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promo) => promo.featured)[0]);
+    return  new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]), 2000);
+    });
   }
 }
