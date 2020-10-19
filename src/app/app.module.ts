909aa3ac4fbdfc1998255927f7437cb4fb654c8d
Mehmet Sökmen <sokmen4083@gmail.com>
2020-10-19 16:16:01 +0200

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,16 @@ import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishDetailComponent
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
